ff93a6c60b3bee58dda05b34fac1aa6d3821ee96
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2018-01-03 16:09:28 -0600

    Fix number interior and exterior length

--- App/Billing/Database/Seeds/ApplicationSeeder.php
+++ App/Billing/Database/Seeds/ApplicationSeeder.php
@@ -19,7 +19,7 @@ class ApplicationSeeder extends InstallSeeder
             'description'=>'Application Billing',
             'nameSpace'=>'Melisa.billing',
             'typeSecurity'=>'art',
-            'version'=>'1.17.3'
+            'version'=>'1.17.4'
         ]);        
     }
     
