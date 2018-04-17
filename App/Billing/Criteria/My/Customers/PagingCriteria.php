c561e9257f6157b6f1fefdf6a8c53c9c83e759b7
Luis Josafat Heredia Contreras <lheredia@yetcargo.com>
2018-04-16 23:38:09 -0500

    fix paging my customers

---
+++ App/Billing/Criteria/My/Customers/PagingCriteria.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Billing\Criteria\My\Customers;
+
+use Melisa\Laravel\Criteria\FilterCriteria;
+use Melisa\core\LogicBusiness;
+use App\Billing\Logics\Repositories\IdentitiesPrivilegeTrait;
+
+/**
+ * 
+ *
+ * @author Luis Josafat Heredia Contreras
+ */
+class PagingCriteria extends FilterCriteria
+{
+    use LogicBusiness;
+    use IdentitiesPrivilegeTrait;
+    
+    public function apply($model, $repository, array $input = [])
+    {        
+        $builder = parent::apply($model, $repository, $input, [
+            'name'=>'c.name'
+        ]);
+        
+        if( isset($input['query'])) {
+            $builder = $builder->where('c.name', 'like', '%' . $input['query'] . '%');
+        }
+        
+        $repositoriesPrivilege = $this->getRepositoriesPrivilege();
+        
+        return $builder
+            ->select([
+                'customers.*',
+                'c.name',
+                'c.rfc',
+                'c.email',
+                'p.name as waytopay',
+                'r.name as repository',
+            ])
+            ->join('repositories as r', 'r.id', '=', 'customers.idRepository')
+            ->join('contributors as c', 'c.id', '=', 'customers.idContributor')
+            ->join('waytopay as p', 'p.id', '=', 'customers.idWaytopay')
+            ->orderBy('c.name')
+            ->whereIn('r.id', $repositoriesPrivilege);        
+    }
+    
+}
